17190ec0af7aad5adf4b1119693f03e3e1b5bacc
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-09-01 20:42:56 -0500

    Update homework.R

--- Mathematical Statistics/homework.R
+++ Mathematical Statistics/homework.R
@@ -2,3 +2,10 @@
 # echang54@jh.edu
 # 09/01/2025
 # HW 1
+
+# In the game of Risk, armies battle via dice throws. Suppose, a defender's position is so weak 
+# that only one 6-sided die can be thrown. The attacker on the other hand has sufficient armies 
+# to justify up to three dice if desired. In a battle, the defending die is compared with the 
+# attacking dice. If any of the attacking dice strictly exceed the defending die, the attacker 
+# wins. Simulate the probability that the attacker wins when choosing to roll [a] 1 die, [b] 2 dice, 
+# or [c] 3 dice. The answer is accessible with conditional probability, but let's simulate it here.
