9746c935691fc697db4a0ddc22d3f535bc3a3050
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-09-01 21:21:02 -0500

    Renamed homework_1 to assignment_1
    Renamed files, added implementation of simulation

--- Mathematical Statistics/assignment_1.R
+++ Mathematical Statistics/assignment_1.R
@@ -1,7 +1,7 @@
 # Ethan Chang, M.S Applied and Computational Mathematics @ JHU
 # echang54@jh.edu
 # 09/01/2025
-# HW 1
+# Assignment 1
 
 # In the game of Risk, armies battle via dice throws. Suppose, a defender's position is so weak 
 # that only one 6-sided die can be thrown. The attacker on the other hand has sufficient armies 
@@ -9,3 +9,21 @@
 # attacking dice. If any of the attacking dice strictly exceed the defending die, the attacker 
 # wins. Simulate the probability that the attacker wins when choosing to roll [a] 1 die, [b] 2 dice, 
 # or [c] 3 dice. The answer is accessible with conditional probability, but let's simulate it here.
+sim_num = 10000
+
+# Function to simulate battles based on how many die the attacker has
+battle <- function(attack_num_dice)
+{
+  defender <- sample(1:6, 1, replace = TRUE)
+  attacker <- sample(1:6, attack_num_dice, replace = TRUE)
+  
+  score <- any(attacker > defender)
+}
+
+# Function to sample battles to find probability of attacker wins
+win_prob <- sapply(1:3, function(k)
+{
+  mean(replicate(sim_num, battle(k)))
+})
+
+win_prob
