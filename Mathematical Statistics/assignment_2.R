50668d40cea06d56d358ae0bed960a6b18dfb773
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-09-08 18:44:31 -0500

    Update assignment_2.R
    Added simulation function for single world series

--- Mathematical Statistics/assignment_2.R
+++ Mathematical Statistics/assignment_2.R
@@ -7,3 +7,18 @@
 # Simulate the probability that team A would win a World Series against team B by simulating 1000 World Series. You many use any software to conduct the simulation. This is easily 
 # derived from a binomial or negative binomial distribution, but I would like you to simulate it on a game x game basis. You may recognize that winning 4 in 4, 5, 6, or 7 games is 
 # equivalent to winning at least 4 in 7 games.
+sim_num = 1000
+
+world_series <- function(A_prob)
+{
+    sample_num = runif(1)
+
+    if (sample_num < A_prob)
+    {
+        return(1)
+    }
+    else
+    {
+        return(0)
+    }
+}
