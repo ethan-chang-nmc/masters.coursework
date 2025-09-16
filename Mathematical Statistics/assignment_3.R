24c36096c786d8bbd99a58c0a2ef9b47acd1f317
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-09-15 22:26:10 -0500

    Update assignment_3.R
    problem description

--- Mathematical Statistics/assignment_3.R
+++ Mathematical Statistics/assignment_3.R
@@ -2,3 +2,15 @@
 # echang54@jh.edu
 # 09/15/2025
 # Assignment 3
+
+# 1. Using the function rbinom in R, generate 1000 random deviates from a B(20,.4) distribution, 
+#compute the sample mean and compare with the theoretical expectation.
+
+
+# 2. Using the function rhyper in R, generate 1000 random deviates from a hypergeometric distribution 
+# with 4 white balls, 8 black balls, and 3 balls drawn without replacement. Compute the sample mean 
+# and compare with the theoretical expectation.
+
+
+# 3. Using the function rexp in R, generate 1000 random deviates from an exponential with lambda=3. 
+# Compute the sample mean and compare with the theoretical expectation, which is 1/lambda.
