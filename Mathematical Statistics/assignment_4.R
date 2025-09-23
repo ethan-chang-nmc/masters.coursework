818e2e127273efdb539c84e0cef853e2298ac90b
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-09-22 22:11:54 -0500

    Update assignment_4.R
    solutions for both problems in assignment 4

--- Mathematical Statistics/assignment_4.R
+++ Mathematical Statistics/assignment_4.R
@@ -12,6 +12,16 @@
 # Find the cdf for the density in problem 3.10.4. Use that to randomly generate a sample of size 5, call it y. 
 # Sort the sample and determine if the interval Y’ 1 to Y’ 5 contains 0.60. The interval is y[1] to y[5]. 
 # Repeat for 1000 trials and compare with your theoretical result.
+trial.4 <- function()
+{
+    unif_samples <- runif(5)
+    y <- sqrt(unif_samples)
+    y_sort <- sort(y)
+    return(y_sort[1] <= 0.6 && y_sort[5] >= 0.6)
+}
+
+results <- replicate(1000, trial.4())
+mean(results)
 
 # MC 3.10.6
 # After determining n analytically from problem 3.10.6, sample n values repeatedly from the distribution of Y. 
@@ -20,3 +30,13 @@
 # of n values, sort the y values and identify the minimum y[1] for comparison to to 0.2. Compute the sample 
 # proportion in 1000 trials of n values to see if the P(Ymin<0.2)>0.9. If your computed n is correct, the 
 # probability should be very close to 0.9.
+trial.6 <- function()
+{
+    u <- runif(12)
+    y <- -log(1 - u)
+    y_min <- min(y)
+    return(y_min < 0.2)
+}
+
+results <- replicate(1000, trial.6())
+mean(results)
