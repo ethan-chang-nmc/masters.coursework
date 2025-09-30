9fb91d6cee5819d1b4db7b8938f379b53f48094e
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-09-29 22:06:59 -0500

    Update assignment_5.R
    problem solutions

--- Mathematical Statistics/assignment_5.R
+++ Mathematical Statistics/assignment_5.R
@@ -11,6 +11,19 @@
 # deviates of W1 on the random deviates of W=X+Y. It won’t be real pretty, but values should follow a line y=x. 
 # Or you could plot histograms of W1 and W on the same graph. (In R you call the two histogram commands and on 
 # the 2nd add the argument, add=T).
+X <- rbinom(500, size = 10, prob = 0.3)
+Y <- rbinom(500, size = 10, prob = 0.3)
+
+W <- X + Y
+
+W1 = rbinom(500, size = 15, prob = 0.3)
+
+W_sorted <- sort(W)
+W1_sorted <- sort(W1)
+
+plot(W_sorted, W1_sorted, main="3.8.2",
+     xlab="Empirical W = X+Y", ylab="Theoretical W1 ~ Bin(15,0.3)",
+     pch=19, col=rgb(0,0,1,0.5))
 
 # Take the same approach for Example 3.8.5, first simulating W=Y/X and then simulating from the distribution 
 # representing the theoretical result of the ratio of Y and X. You need a little more guidance here. By Example 
@@ -26,3 +39,19 @@
 # U(0,1) random variable. We use this fact to establish random deviates. So we equate a uniform random number r with 
 # the distribution function F(w). You can verify this, by getting F(w) for yourself, but it works out to be r=1-1/(1+w). 
 # Solving for w gives the relationship above. That is how most random number generators are established for specific distributions!
+X <- rexp(2000, rate = 1)
+Y <- rexp(2000, rate = 1)
+
+W <- Y/X
+
+r <- runif(2000)
+W_dev <- (1/(1-r)) - 1
+
+cutoff <- floor(0.95* length(W))
+W_sorted <- sort(W)[1:cutoff]
+W_dev_sorted <- sort(W_dev)[1:cutoff]
+
+plot(W_ratio_sorted, W_dev_sorted, main="3.8.5",
+     xlab="Empirical W = Y/X", ylab="Theoretical W_dev",
+     pch=19, col=rgb(0,0.5,0,0.5))
+abline(0,1,col="red", lwd=2)  # y=x reference line
