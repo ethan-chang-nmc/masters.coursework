c29218b74bf1c534454caebf88163cdd5a6d8897
Ethan Chang <71246919+ethan-chang-nmc@users.noreply.github.com>
2025-10-21 21:51:33 -0500

    Update assignment_8.R
    problem description

--- Mathematical_Statistics/assignment_8.R
+++ Mathematical_Statistics/assignment_8.R
@@ -2,3 +2,9 @@
 # echang54@jh.edu
 # 10/21/2025
 # Assignment 8
+
+# Review example 5.4.5 on page 314 in the text. Simulate the estimators mu_hat_1 and mu_hat_2, 
+# 1000 times using a normal distribution with mean 10 and standard deviation 5. Each of your 
+# three random sample values will come from this distribution. Form the estimates and compare 
+# to the theoretical expectation (discussed in Example 5.4.3) and theoretical variance given in 
+# Example 5.4.5. You will be using the function rnorm in R.
